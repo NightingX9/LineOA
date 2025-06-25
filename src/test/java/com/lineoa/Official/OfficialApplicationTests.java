06c78209aa41b69533b8b7bf4bfef722d4296fc4
Nightkansak <Kansakc@pccth.com>
2025-06-25 12:33:49 +0700

    dev

---
+++ src/test/java/com/lineoa/Official/OfficialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lineoa.Official;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
